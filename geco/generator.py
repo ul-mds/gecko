dc5b6bfc1649486e0b9dabcbc912022b376db477
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2023-09-21 15:45:20 +0200

    Add generator with dependencies between columns

--- geco/generator.py
+++ geco/generator.py
@@ -38,7 +38,6 @@ def from_frequency_table(
         # setup local vars
         value_list: list[str] = []
         abs_freq_list: list[int] = []
-        total_abs_freq = 0
 
         # skip header row
         if header:
@@ -55,13 +54,75 @@ def from_frequency_table(
 
             value_list.append(line_val)
             abs_freq_list.append(line_freq)
-            total_abs_freq += line_freq
 
-    rel_freq_list = [freq / total_abs_freq for freq in abs_freq_list]
+    # this works because abs_freq_list is broadcast to a numpy array
+    # noinspection PyUnresolvedReferences
+    rel_freq_list = abs_freq_list / np.sum(abs_freq_list)
 
     # return type can be treated as a list
     # noinspection PyTypeChecker
     def _generate(count: int) -> list[list[str]]:
         return [rng.choice(value_list, count, p=rel_freq_list)]
+
+    return _generate
+
+
+def from_multicolumn_frequency_table(
+        csv_file_path: Path,
+        encoding: str = "utf-8",
+        delimiter: str = ",",
+        rng: Generator | None = None,
+        column_names: str | list[str] | None = None,
+        count_column_name: str = "count"
+) -> GeneratorFunc:
+    if column_names is None:
+        raise ValueError("column names must be defined")
+
+    if type(column_names) is list and len(column_names) == 0:
+        raise ValueError("list of column names may not be empty")
+
+    if type(column_names) is str:
+        column_names = [column_names]
+
+    if rng is None:
+        rng = np.random.default_rng()
+
+    with csv_file_path.open(mode="r", encoding=encoding, newline="") as f:
+        # create reader instance
+        reader = csv.DictReader(f, delimiter=delimiter)
+
+        # sanity check
+        for col_name in column_names + [count_column_name]:
+            if col_name not in reader.fieldnames:
+                raise ValueError(f"column `{col_name}` is not present in the CSV file")
+
+        column_value_list: list[list] = []
+        abs_freq_list: list[int] = []
+
+        for row in reader:
+            # rows can be indexed using strings. no idea why the type checker complains here.
+            # noinspection PyTypeChecker
+            row_freq = int(row[count_column_name])
+
+            if row_freq < 0:
+                raise ValueError("absolute frequency must not be negative")
+
+            # see above
+            # noinspection PyTypeChecker
+            row_tuple = list(row[col_name] for col_name in column_names)
+
+            column_value_list.append(row_tuple)
+            abs_freq_list.append(row_freq)
+
+    # this works because abs_freq_list is broadcast to a numpy array
+    # noinspection PyUnresolvedReferences
+    rel_freq_list = abs_freq_list / np.sum(abs_freq_list)
+
+    def _generate(count: int) -> list[list[str]]:
+        # choice() can work with lists of tuples
+        # noinspection PyTypeChecker
+        x = rng.choice(column_value_list, count, p=rel_freq_list)
+        # type conversion because zip will return an iterator over tuples, but we need lists instead
+        return list(list(t) for t in zip(*x))
 
     return _generate
