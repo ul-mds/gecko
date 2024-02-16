19012d7d1e060ad1b018c906839be2c2e8327ecc
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2024-02-16 13:33:52 +0100

    Expand fruit benchmark suite

--- tests/test_bench_fruits.py
+++ tests/test_bench_fruits.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from gecko import generator
+from gecko import generator, corruptor
 from tests.helpers import get_asset_path
 
 pytestmark = pytest.mark.benchmark
@@ -113,3 +113,88 @@ def test_bench_to_dataframe(benchmark, rng):
 
     for count in record_counts:
         benchmark(__gen_dataframe(count), name=f"gen_dataframe_fruits({count})")
+
+
+def test_bench_corrupt_edit(benchmark, rng):
+    gen_origin = __create_origin_single_column_generator(rng)
+    corr_edit = corruptor.with_edit(rng=rng)
+
+    for count in record_counts:
+        srs_lst = gen_origin(count)
+        benchmark(
+            lambda: corr_edit(srs_lst), name=f"corr_edit_fruits({count})", calls=100
+        )
+
+
+def test_bench_corrupt_missing(benchmark, rng):
+    gen_origin = __create_origin_single_column_generator(rng)
+    corr_missing = corruptor.with_missing_value(strategy="all")
+
+    for count in record_counts:
+        srs_lst = gen_origin(count)
+        benchmark(
+            lambda: corr_missing(srs_lst),
+            name=f"corr_missing_fruits({count})",
+            calls=100,
+        )
+
+
+def test_bench_corrupt_replacement_table(benchmark, rng):
+    gen_origin = __create_origin_single_column_generator(rng)
+    corr_replacement = corruptor.with_replacement_table(
+        get_asset_path("ocr.csv"),
+        rng=rng,
+    )
+
+    for count in record_counts:
+        srs_lst = gen_origin(count)
+        benchmark(
+            lambda: corr_replacement(srs_lst),
+            name=f"corr_replacement_fruits({count})",
+            calls=100,
+        )
+
+
+def test_bench_corrupt_cldr(benchmark, rng):
+    gen_origin = __create_origin_single_column_generator(rng)
+    corr_cldr = corruptor.with_cldr_keymap_file(
+        get_asset_path("de-t-k0-windows.xml"),
+        rng=rng,
+    )
+
+    for count in record_counts:
+        srs_lst = gen_origin(count)
+        benchmark(
+            lambda: corr_cldr(srs_lst), name=f"corr_cldr_fruits({count})", calls=100
+        )
+
+
+def test_bench_permute(benchmark, rng):
+    gen_fruit_type = __create_fruit_type_multicolumn_generator(rng)
+    corr_permute = corruptor.with_permute()
+
+    for count in record_counts:
+        srs_lst = gen_fruit_type(count)
+        benchmark(
+            lambda: corr_permute(srs_lst),
+            name=f"corr_permute_fruits({count})",
+            calls=100,
+        )
+
+
+def test_bench_categorical(benchmark, rng):
+    gen_origin = __create_origin_single_column_generator(rng)
+    corr_categorical = corruptor.with_categorical_values(
+        get_asset_path("freq-fruit-origin.csv"),
+        header=True,
+        value_column="origin",
+        rng=rng,
+    )
+
+    for count in record_counts:
+        srs_lst = gen_origin(count)
+        benchmark(
+            lambda: corr_categorical(srs_lst),
+            name=f"corr_categorical_fruits({count})",
+            calls=100,
+        )
