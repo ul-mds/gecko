b09e02ea282e39f787d2543415dfab79957992cd
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2024-11-08 13:18:29 +0100

    Add tests for updated `with_replacement_table`

--- tests/test_mutator_with_p.py
+++ tests/test_mutator_with_p.py
@@ -1,4 +1,5 @@
 import string
+
 import pandas as pd
 import pytest
 
@@ -8,7 +9,7 @@ from gecko.mutator import (
     with_missing_value,
     with_replacement_table,
 )
-from tests.helpers import get_asset_path
+from tests.helpers import get_asset_path, random_strings, write_temporary_csv_file
 
 
 def test_with_cldr_keymap_file(rng):
@@ -87,16 +88,107 @@ def test_with_missing_value_existing(rng):
 
 
 def test_with_replacement_table(rng):
-    srs = pd.Series(["xyz", "foo", "bar", "baz", "bat"])
-    mut_replace = with_replacement_table(
-        pd.DataFrame.from_dict(
-            {"source": ["o", "b", "b", "a"], "target": ["O", "B", "H", "A"]}
-        ),
-        source_column="source",
-        target_column="target",
-        inline=True,
-        rng=rng,
+    srs = pd.Series(list(string.ascii_lowercase))
+
+    # replacement table that maps lowercase chars to uppercase
+    df = pd.DataFrame(
+        list(zip(string.ascii_lowercase, string.ascii_uppercase)),
+        columns=["source", "target"],
     )
 
-    srs_out = mut_replace([srs], 1)
-    print(srs_out)
+    mut_replacement_table = with_replacement_table(
+        df, source_column="source", target_column="target", rng=rng
+    )
+
+    (srs_mut,) = mut_replacement_table([srs], 1)
+
+    assert len(srs) == len(srs_mut)
+    assert (srs != srs_mut).all()
+    assert (srs.str.len() == srs_mut.str.len()).all()
+
+
+def test_with_replacement_table_reverse(rng):
+    # lowercase AND uppercase will be converted to the opposite case
+    srs = pd.Series(list(string.ascii_lowercase + string.ascii_uppercase))
+
+    df = pd.DataFrame(
+        list(zip(string.ascii_lowercase, string.ascii_uppercase)),
+        columns=["source", "target"],
+    )
+
+    # this mutator should ensure that uppercase -> lowercase should take place
+    mut_replacement_table = with_replacement_table(
+        df, source_column="source", target_column="target", reverse=True, rng=rng
+    )
+
+    (srs_mut,) = mut_replacement_table([srs], 1)
+
+    assert len(srs) == len(srs_mut)
+    assert (srs != srs_mut).all()
+    assert (srs.str.len() == srs_mut.str.len()).all()
+
+
+def test_with_replacement_table_inline(rng):
+    # generate random lowercase strings
+    srs = pd.Series(random_strings(str_len=10, charset=string.ascii_lowercase, rng=rng))
+
+    df = pd.DataFrame(
+        list(zip(string.ascii_lowercase, string.ascii_uppercase)),
+        columns=["source", "target"],
+    )
+
+    # this mutator should ensure that replacements occur inline
+    mut_replacement_table = with_replacement_table(
+        df, source_column="source", target_column="target", inline=True, rng=rng
+    )
+
+    (srs_mut,) = mut_replacement_table([srs], 1)
+
+    assert len(srs) == len(srs_mut)
+    assert (srs != srs_mut).all()
+    assert (srs.str.len() == srs_mut.str.len()).all()
+
+
+def test_with_replacement_table_warn_p(rng):
+    srs = pd.Series(["a"] * 50 + ["b"] * 50)
+
+    # no mapping for "b"
+    df = pd.DataFrame.from_dict({"source": ["a"], "target": ["A"]})
+
+    mut_replacement_table = with_replacement_table(
+        df, source_column="source", target_column="target", rng=rng
+    )
+
+    with pytest.warns(PNotMetWarning) as record:
+        (srs_mut,) = mut_replacement_table([srs], 0.8)
+
+    assert len(record) == 1
+    assert (
+        record[0]
+        .message.args[0]
+        .startswith("with_replacement_table: desired probability of 0.8 cannot be met")
+    )
+
+    assert len(srs) == len(srs_mut)
+    assert (srs.iloc[:50] != srs_mut.iloc[:50]).all()
+    assert (srs.iloc[50:] == srs_mut.iloc[50:]).all()
+    assert (srs.str.len() == srs_mut.str.len()).all()
+
+
+def test_with_replacement_table_csv(rng, tmp_path):
+    srs = pd.Series(list(string.ascii_lowercase))
+    csv_file_path = write_temporary_csv_file(
+        tmp_path,
+        header=["source", "target"],
+        rows=list(zip(string.ascii_lowercase, string.ascii_uppercase)),
+    )
+
+    mut_replacement_table = with_replacement_table(
+        csv_file_path, source_column="source", target_column="target", rng=rng
+    )
+
+    (srs_mut,) = mut_replacement_table([srs], 1)
+
+    assert len(srs) == len(srs_mut)
+    assert (srs != srs_mut).all()
+    assert (srs.str.len() == srs_mut.str.len()).all()
